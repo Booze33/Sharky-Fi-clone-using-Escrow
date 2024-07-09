4c380e3a5170e3a18c80a50f4f3dd0ca77c93532
Booze33 <tebetisloh@gmail.com>
2024-07-10 01:27:32 +0200

    project

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("38mFsa4ZPpRLugnbySeAurd6cKcbTP91c8YzswqdzjK5");
 
 #[program]
 pub mod nft_lend_borrow {
